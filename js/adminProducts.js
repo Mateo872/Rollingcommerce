5f858322e32a30606161a6c44d36705e0fbc68e9
Ana Rocha <anasoledad.rocha@gmail.com>
2023-05-06 13:18:20 -0300

    Se agregó y cambio los id de los inputs

--- js/adminProducts.js
+++ js/adminProducts.js
@@ -1,13 +1,27 @@
 import Product from "./addProduct.js";
+import { dataValidate } from "./helpers.js";
 
 
 const btnAdd = document.getElementById('btnAdd');
 
+const title = document.getElementById('title');
+const code = document.getElementById('code');
+const image = document.getElementById('image');
+const imagePreviewOne = document.getElementById('image1');
+const imagePreviewTwo = document.getElementById('image2');
+const imagePreviewThree = document.getElementById('image3');
+const price = document.getElementById('price');
+const description = document.getElementById('description');
+const characteristics = document.getElementById('characteristics');
+const stock = document.getElementById('stock');
+const category = document.getElementById('category')
+const form = document.getElementById('form');
+const msjFormulario = document.getElementById('msjForm');
 
 const modalProduct = new bootstrap.Modal(document.querySelector('#modalAdd'));
 
 btnAdd.addEventListener('click', showModalProduct);
-// form.addEventListener('submit', loadProduct);
+form.addEventListener('submit', loadProduct);
 
 function showModalProduct(){
   //abrir la ventana modal
@@ -32,16 +46,17 @@ function createProduct() {
 function loadProduct(e) {
   e.preventDefault();
 
+  console.log('creando la producto')
   let data = dataValidate(
     title.value,
-    description.value,
     price.value,
-    category.value,
     characteristics.value,
     imagePreviewOne.value,
     imagePreviewTwo.value,
     imagePreviewThree.value,
-    stock.value
+    description.value,
+    category.value,
+    stock.value,
   );
   if (data.length === 0) {
     let newProduct = new Product(
@@ -58,11 +73,12 @@ function loadProduct(e) {
     );
 
     listProducts.push(newProduct);
+    modalProduct.hide();
 
     saveLS();
   } else {
-    msjFormulario.className = 'alert alert-danger mt-3';
-    msjFormulario.innerHTML = data;
+    msjForm.className = 'alert alert-danger mt-3';
+    msjForm.innerHTML = data;
   }
 }
 
