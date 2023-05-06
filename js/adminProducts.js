036a141a47db582b1374427fb5d45f368b981191
Ana Rocha <anasoledad.rocha@gmail.com>
2023-05-06 05:39:09 -0300

    Se agregó la funcionalidad de abrir ventana modal desde js

--- js/adminProducts.js
+++ js/adminProducts.js
@@ -1,7 +1,22 @@
 import Product from "./addProduct.js";
 
+
+const btnAdd = document.getElementById('btnAdd');
+
+
+const modalProduct = new bootstrap.Modal(document.querySelector('#modalAdd'));
+
+btnAdd.addEventListener('click', showModalProduct);
+// form.addEventListener('submit', loadProduct);
+
+function showModalProduct(){
+  //abrir la ventana modal
+  modalProduct.show();
+  console.log('aqui vamos a crear una peli')
+}
+
 function createProduct() {
-  let nuevaPeli = new Product(
+  let newProduct = new Product(
     "Astro A30",
     "Los mejores auriculares",
     "urlImage",
@@ -20,16 +35,16 @@ function loadProduct(e) {
   let data = dataValidate(
     title.value,
     description.value,
-    image.value,
     price.value,
     category.value,
     characteristics.value,
     imagePreviewOne.value,
     imagePreviewTwo.value,
-    imagePreviewThree.value
+    imagePreviewThree.value,
+    stock.value
   );
   if (data.length === 0) {
-    let newProduct = new Pelicula(
+    let newProduct = new Product(
       title.value,
       description.value,
       image.value,
@@ -38,13 +53,16 @@ function loadProduct(e) {
       characteristics.value,
       imagePreviewOne.value,
       imagePreviewTwo.value,
-      imagePreviewThree.value
+      imagePreviewThree.value,
+      stock.value
     );
 
-    listProducts.push(nuevaPeli);
+    listProducts.push(newProduct);
 
     saveLS();
   } else {
+    msjFormulario.className = 'alert alert-danger mt-3';
+    msjFormulario.innerHTML = data;
   }
 }
 
