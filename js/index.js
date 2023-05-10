b02bc19e7a9f85f26e54c62c457e9f9752db7125
Mario-Chavez <marioachavez1@gmail.com>
2023-05-09 21:08:04 -0300

    se guardo el user en el sessionStorage

--- js/index.js
+++ js/index.js
@@ -6,17 +6,24 @@ const userPassword = document.getElementById("pass");
 const msjForm = document.getElementById("msjFormIndex");
 const modalUser = new bootstrap.Modal(document.querySelector("#login"));
 
+const email = "administrador@gmail.com";
+const pass = "admin123";
+let newUser = [];
+
 let listProducts = JSON.parse(localStorage.getItem("listProducts")) || [];
+
+let userInSeesion = sessionStorage.getItem("user");
+console.log(userInSeesion);
 
 formLogin.addEventListener("submit", loadUser);
 
 listProducts.map((product) => {
-  createCard(product);
+    createCard(product);
 });
 
 function createCard(product) {
-  let productCardData = document.getElementById("cards");
-  productCardData.innerHTML += `<div class="product d-flex flex-column justify-content-between p-3">
+    let productCardData = document.getElementById("cards");
+    productCardData.innerHTML += `<div class="product d-flex flex-column justify-content-between p-3">
   <i class="bi bi-heart"></i>
   <div class="product_image d-flex justify-content-center">
       <img src="${product.image}" alt="${product.title}" />
@@ -37,23 +44,40 @@ function createCard(product) {
 `;
 }
 
-function loadUser(e) {
-  e.preventDefault();
+function validarUser() {}
 
-  let data = dataLoginValidate(userEmail.value, userPassword.value);
-  if (data.length === 0) {
-    Swal.fire(
-      "Felicidades Te Registraste",
-      "Ya puedes acceder a tu cuenta",
-      "success"
-    );
-    formLogin.reset();
-    modalUser.hide();
-  } else {
-    msjForm.className = "alert alert-danger mt-3";
-    msjForm.innerHTML = data;
-    setTimeout(() => {
-      msjForm.className = "d-none";
-    }, 4000);
-  }
+function loadUser(e) {
+    e.preventDefault();
+    if (userEmail.value === email && userPassword.value === pass) {
+        msjForm.className = "alert alert-info mt-3";
+        msjForm.innerHTML = "Te llevaremos a tus productos";
+        setTimeout(() => {
+            msjForm.className = "d-none";
+        }, 4000);
+    } else {
+        let data = dataLoginValidate(userEmail.value, userPassword.value);
+        if (data.length === 0) {
+            Swal.fire(
+                "Felicidades Te Registraste",
+                "Ya puedes acceder a tu cuenta",
+                "success"
+            );
+            let userMail = userEmail.value;
+            let userPass = userPassword.value;
+            saveSessionStorage(userMail, userPass);
+            formLogin.reset();
+            modalUser.hide();
+        } else {
+            msjForm.className = "alert alert-danger mt-3";
+            msjForm.innerHTML = data;
+            setTimeout(() => {
+                msjForm.className = "d-none";
+            }, 4000);
+        }
+    }
+}
+
+function saveSessionStorage(user, pass) {
+    newUser.push(user, pass);
+    sessionStorage.setItem("user", JSON.stringify(newUser));
 }
