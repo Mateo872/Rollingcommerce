f93dba2c6cdd19b7f48ddc1a29bff460d60a2cc5
Mario-Chavez <marioachavez1@gmail.com>
2023-05-10 00:19:52 -0300

    se realizo el cambio de icon para el administrador

--- js/index.js
+++ js/index.js
@@ -4,25 +4,32 @@ const formLogin = document.getElementById("form-login");
 const userEmail = document.getElementById("email");
 const userPassword = document.getElementById("pass");
 const msjForm = document.getElementById("msjFormIndex");
+const iconEdit = document.getElementById("iconEdit");
+const iconUser = document.getElementById("iconUser");
+
 const modalUser = new bootstrap.Modal(document.querySelector("#login"));
 let userInSeesion = JSON.parse(sessionStorage.getItem("user"));
 let email = "administrador@gmail.com";
 let pass = "admin123";
 let newUser = [];
-
-formLogin.addEventListener("submit", loadUser);
-
+let userOn = false;
 let listProducts = JSON.parse(localStorage.getItem("listProducts")) || [];
 
+if (userInSeesion) {
+    userOn = true;
+    changeIcon();
+}
+
+formLogin.addEventListener("submit", loadUser);
 formLogin.addEventListener("submit", loadUser);
 
 listProducts.map((product) => {
-  createCard(product);
+    createCard(product);
 });
 
 function createCard(product) {
-  let productCardData = document.getElementById("cards");
-  productCardData.innerHTML += `<div class="product d-flex flex-column justify-content-between p-3">
+    let productCardData = document.getElementById("cards");
+    productCardData.innerHTML += `<div class="product d-flex flex-column justify-content-between p-3">
   <i class="bi bi-heart"></i>
   <div class="product_image d-flex justify-content-center">
       <img src="${product.image}" alt="${product.title}" />
@@ -44,43 +51,58 @@ function createCard(product) {
 }
 
 function loadUser(e) {
-  e.preventDefault();
-  if (userEmail.value === email && userPassword.value === pass) {
-    msjForm.className = "alert alert-info mt-3";
-    msjForm.innerHTML = "Te llevaremos a tus productos";
-    setTimeout(() => {
-      msjForm.className = "d-none";
-      changePage();
-    }, 4000);
-  } else {
-    let data = dataLoginValidate(userEmail.value, userPassword.value);
-
-    if (data.length === 0) {
-      Swal.fire(
-        "Felicidades Te Registraste",
-        "Ya puedes acceder a tu cuenta",
-        "success"
-      );
-      let userMail = userEmail.value;
-      let userPass = userPassword.value;
-      saveSessionStorage(userMail, userPass);
-      formLogin.reset();
-      modalUser.hide();
+    e.preventDefault();
+    if (userEmail.value === email && userPassword.value === pass) {
+        msjForm.className = "alert alert-info mt-3";
+        msjForm.innerHTML = "Te llevaremos a tus productos";
+        userOn = true;
+        changeIcon();
+        setTimeout(() => {
+            msjForm.className = "d-none";
+            changePage();
+        }, 4000);
     } else {
-      msjForm.className = "alert alert-danger mt-3";
-      msjForm.innerHTML = data;
-      setTimeout(() => {
-        msjForm.className = "d-none";
-      }, 4000);
+        let data = dataLoginValidate(userEmail.value, userPassword.value);
+
+        if (data.length === 0) {
+            Swal.fire(
+                "Felicidades Te Registraste",
+                "Ya puedes acceder a tu cuenta",
+                "success"
+            );
+            let userMail = userEmail.value;
+            let userPass = userPassword.value;
+            saveSessionStorage(userMail, userPass);
+            formLogin.reset();
+            modalUser.hide();
+            userOn = true;
+            changeIcon();
+        } else {
+            msjForm.className = "alert alert-danger mt-3";
+            msjForm.innerHTML = data;
+            setTimeout(() => {
+                msjForm.className = "d-none";
+            }, 4000);
+        }
     }
-  }
 }
 
 function saveSessionStorage(user, pass) {
-  newUser.push(user, pass);
-  sessionStorage.setItem("user", JSON.stringify(newUser));
+    newUser.push(user, pass);
+    sessionStorage.setItem("user", JSON.stringify(newUser));
 }
 
 function changePage() {
-  window.location.href = "./pages/admin.html";
+    window.location.href = "./pages/admin.html";
+}
+function changeIcon() {
+    if (userOn === true) {
+        console.log("entre a true");
+        iconEdit.className = "nav-item";
+        iconUser.className = "nav-item d-none ";
+    } else {
+        console.log("entre a false");
+        iconEdit.className = "nav-item d-none";
+        iconUser.className = "nav-item ";
+    }
 }
